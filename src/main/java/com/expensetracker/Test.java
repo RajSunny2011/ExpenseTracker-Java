8fc328f6937cf1bd400f230ca658905506bfa926
Ishita <ishitasharma6119@gmail.com>
2025-03-11 20:25:55 +0530

    added Test.java

---
+++ src/main/java/com/expensetracker/Test.java
@@ -0,0 +1,11 @@
+package main.java.com.expensetracker;
+public class Test {
+    public static void main(String[] args) {
+        DatabaseManager db = new DatabaseManager("database.csv");
+        Transaction t1 = new Transaction("2021-01-01", "Income", "Salary", 50000, "Salary for January 2021");
+        Transaction t2 = new Transaction("2021-01-02", "Expense", "Rent", 10000, "Rent for January 2021");
+        db.addTransaction(t1);
+        db.addTransaction(t2);
+        db.viewAllTransactions();
+    }
+}
